293b9fe526010bddb03ac8fddfb821fefe288294
ikichrs <christiandastin@gmail.com>
2022-12-19 11:55:42 +0700

    crud airplane,payment,schedule,seats

---
+++ src/main/java/com/binar/tix/controller/AirplaneController.java
@@ -0,0 +1,122 @@
+package com.binar.tix.controller;
+
+import com.binar.tix.payload.Messages;
+import com.binar.tix.payload.ReqCreateAirplane;
+import com.binar.tix.service.AirplaneService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/airplane")
+public class AirplaneController {
+
+    @Autowired
+    AirplaneService airplaneService;
+
+    @Operation(summary="Menambahkan Pesawat")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Pesawat berhasil ditambahkan.",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "400", description = "Eror dari sisi Browser .",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "500", description = "Eror dari sisi Server.",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))})})
+    @PostMapping("/add-airplane")
+    public ResponseEntity<Messages> create(@RequestBody ReqCreateAirplane airplane){
+        airplaneService.saveAirplane(airplane);
+        Messages messages= new Messages();
+        messages.setResponseCode(201);
+        messages.setResponseMessage("Berhasil Ditambahkan");
+        return ResponseEntity.ok().body(messages);
+    }
+
+    @Operation(summary="Menampilkan Pesawat")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "", description = ".",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "400", description = "Eror dari sisi Browser .",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "500", description = "Eror dari sisi Server.",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))})})
+    @GetMapping("/get-airplane")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseEntity<Messages> get(){
+        Messages messages= new Messages();
+        messages.setResponseCode(200);
+        messages.setResponseMessage("Berhasil Ditampilkan");
+        messages.setData(airplaneService.findAllAirplanes());
+        return ResponseEntity.ok().body(messages);
+    }
+
+    @Operation(summary="Mengupdate Data Pesawat")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "", description = ".",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "400", description = "Eror dari sisi Browser .",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "500", description = "Eror dari sisi Server.",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))})})
+    @PutMapping("/update-airplane/{airplaneId}")
+    public ResponseEntity<Messages> update(@PathVariable Integer airplaneId, @RequestBody ReqCreateAirplane airplane){
+        Messages messages= new Messages();
+        Boolean status = airplaneService.updateAirplane(airplaneId, airplane);
+        if (Boolean.TRUE.equals(status)) {
+            airplaneService.updateAirplane(airplaneId, airplane);
+            messages.setResponseCode(200);
+            messages.setResponseMessage("Berhasil Diupdate");
+        }
+        else {
+            messages.setResponseCode(204);
+            messages.setResponseMessage("Data tidak ditemukan");
+        }
+        return ResponseEntity.ok().body(messages);
+    }
+
+    @Operation(summary="Menghapus Data Pesawat")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "", description = ".",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "400", description = "Eror dari sisi Browser .",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "500", description = "Eror dari sisi Server.",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))})})
+    @DeleteMapping("/delete-airplane/{airplaneId}")
+    public ResponseEntity<Messages> delete(@PathVariable Integer airplaneId){
+        Messages messages= new Messages();
+        Boolean status = airplaneService.deleteAirplane(airplaneId);
+        if (Boolean.TRUE.equals(status)) {
+            airplaneService.deleteAirplane(airplaneId);
+            messages.setResponseCode(200);
+            messages.setResponseMessage("Berhasil Dihapus");
+        }
+        else {
+            messages.setResponseCode(204);
+            messages.setResponseMessage("Data tidak ditemukan");
+        }
+        return ResponseEntity.ok().body(messages);
+    }
+}
+
---
+++ src/main/java/com/binar/tix/controller/PaymentController.java
@@ -0,0 +1,125 @@
+package com.binar.tix.controller;
+
+import com.binar.tix.payload.Messages;
+import com.binar.tix.payload.ReqCreatePayment;
+import com.binar.tix.service.PaymentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @Autowired
+    PaymentService paymentService;
+
+    @Operation(summary="Menambahkan Pembayaran")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Pembayaran berhasil ditambahkan.",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "400", description = "Eror dari sisi Browser .",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "500", description = "Eror dari sisi Server.",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))})})
+    @PostMapping("/add-payment")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Messages> create(@RequestBody ReqCreatePayment payment){
+        paymentService.savePayment(payment);
+        Messages messages= new Messages();
+        messages.success();
+        messages.setResponseCode(201);
+        messages.setResponseMessage("Berhasil Ditambahkan");
+        return ResponseEntity.ok().body(messages);
+    }
+
+    @Operation(summary="Menampilkan Pembayaran")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "", description = ".",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "400", description = "Eror dari sisi Browser .",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "500", description = "Eror dari sisi Server.",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))})})
+    @GetMapping("/get-payment")
+    public ResponseEntity<Messages> get(){
+        Messages messages= new Messages();
+        messages.success();
+        messages.setResponseCode(200);
+        messages.setResponseMessage("Berhasil Ditampilkan");
+        messages.setData(paymentService.findAllPayment());
+        return ResponseEntity.ok().body(messages);
+    }
+
+    @Operation(summary="Mengupdate Data Pembayaran")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "", description = ".",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "400", description = "Eror dari sisi Browser .",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "500", description = "Eror dari sisi Server.",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))})})
+    @PutMapping("/update-payment/{paymentId}")
+    public ResponseEntity<Messages> update(@PathVariable Integer paymentId, @RequestBody ReqCreatePayment payment){
+        Messages messages= new Messages();
+        Boolean status = paymentService.updatePayment(paymentId, payment);
+        if (Boolean.TRUE.equals(status)) {
+            paymentService.updatePayment(paymentId, payment);
+            messages.setResponseCode(200);
+            messages.setResponseMessage("Berhasil Diupdate");
+        }
+        else {
+            messages.setResponseCode(204);
+            messages.setResponseMessage("Data tidak ditemukan");
+        }
+        return ResponseEntity.ok().body(messages);
+    }
+
+    @Operation(summary="Menghapus Data Pembayaran")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "", description = ".",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "400", description = "Eror dari sisi Browser .",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))}),
+            @ApiResponse(responseCode = "500", description = "Eror dari sisi Server.",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = Response.class))})})
+    @DeleteMapping("/delete-payment/{paymentId}")
+    public ResponseEntity<Messages> delete(@PathVariable Integer paymentId){
+        Messages messages= new Messages();
+        Boolean status = paymentService.deletePayment(paymentId);
+        if (Boolean.TRUE.equals(status)) {
+            paymentService.deletePayment(paymentId);
+            messages.success();
+            messages.setResponseCode(200);
+            messages.setResponseMessage("Berhasil Dihapus");
+        }
+        else {
+            messages.notFound();
+            messages.setResponseCode(204);
+            messages.setResponseMessage("Data tidak ditemukan");
+        }
+        return ResponseEntity.ok().body(messages);
+    }
+}
