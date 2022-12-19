878e42e407760399c30ed04c1f431ac4849a3b48
rikojanuarsawaludin@gmail.com <rikojanuarsawaludin@gmail.com>
2022-12-19 10:18:59 +0700

    Admin, Crud Payment, Swagger Config

--- src/main/java/com/binar/tix/utility/Constant.java
+++ src/main/java/com/binar/tix/utility/Constant.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 public class Constant {
 
     private Constant() {}
-
+    public static final String AUTH = "Authorization";
     public  static final String ENCRYPT_KEY = "44F4iTX9nplIpohdCXMh8GijF6iM1aGZooY";
     public  static final String DOMESTIK = "domestik";
     public  static final String INTER = "international";
