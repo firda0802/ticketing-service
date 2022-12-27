8c4c3883c8aaefddd18a05f4be23f811272145b5
firda agustina kusumaningrum <firda080802@gmail.com>
2022-12-27 13:17:06 +0800

    up swagger

--- src/main/java/com/binar/tix/controller/ClassController.java
+++ src/main/java/com/binar/tix/controller/ClassController.java
@@ -11,7 +11,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -27,6 +43,17 @@ public class ClassController {
     CrudService classService;
     private final ObjectMapper mapper = new ObjectMapper();
 
+    @Operation(responses = {
+            @ApiResponse(responseCode = "200", content = @Content(examples = {
+                    @ExampleObject(name = "Class List", description = "Menampilkan list kelas beserta nama dan harga untuk para penumpang berdasarkan id.", value = "{\n"
+                            +
+                            "    \"responseCode\": 200,\n" +
+                            "    \"responseMessage\": \"Sukses\",\n" +
+                            "    \"data\": \"classId\": 1, \n" +
+                            "name\": Economi Class\"\n" +
+                            "price\": 35.000\"\n" +
+                            "}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)) })
     @GetMapping(value = "/get-list")
     public ResponseEntity<Messages> listClass(HttpServletRequest httpServletRequest) throws JsonProcessingException {
         String writeLog = HttpUtility.writeLogRequest(httpServletRequest, mapper.writeValueAsString("-"));
@@ -39,15 +66,24 @@ public class ClassController {
         return ResponseEntity.ok().body(resp);
     }
 
+    @Operation(responses = {
+            @ApiResponse(responseCode = "200", content = @Content(examples = {
+                    @ExampleObject(name = "Add Update Class", description = "Dapat Melakukan update pada kelas di aplikasi.", value = "{\n"
+                            +
+                            "    \"responseCode\": 200,\n" +
+                            "    \"responseMessage\": \"Sukses\",\n" +
+                            "}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)) })
     @PostMapping(value = "/add-update-class")
-    public ResponseEntity<Messages> addUpdateClass(@RequestBody ReqUpdateClass req, HttpServletRequest httpServletRequest) throws JsonProcessingException {
+    public ResponseEntity<Messages> addUpdateClass(@RequestBody ReqUpdateClass req,
+            HttpServletRequest httpServletRequest) throws JsonProcessingException {
         String writeLog = HttpUtility.writeLogRequest(httpServletRequest, mapper.writeValueAsString(req));
         log.info(writeLog);
         boolean status = classService.updateAddClass(req);
         Messages resp = new Messages();
-        if(status){
+        if (status) {
             resp.success();
-        }else{
+        } else {
             resp.notFound();
         }
 
@@ -56,8 +92,17 @@ public class ClassController {
         return ResponseEntity.ok().body(resp);
     }
 
+    @Operation(responses = {
+            @ApiResponse(responseCode = "200", content = @Content(examples = {
+                    @ExampleObject(name = "Delete Class", description = "Dapat menghapus kelas jika id tersedia.", value = "{\n"
+                            +
+                            "    \"responseCode\": 200,\n" +
+                            "    \"responseMessage\": \"Sukses\",\n" +
+                            "}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)) })
     @DeleteMapping(value = "/delete/{classId}")
-    public ResponseEntity<Messages> deleteClass(@PathVariable(name = "classId") Integer classId, HttpServletRequest httpServletRequest) throws JsonProcessingException {
+    public ResponseEntity<Messages> deleteClass(@PathVariable(name = "classId") Integer classId,
+            HttpServletRequest httpServletRequest) throws JsonProcessingException {
         String writeLog = HttpUtility.writeLogRequest(httpServletRequest, mapper.writeValueAsString(classId));
         log.info(writeLog);
         Messages resp = new Messages();
